8ba5c1178035322b3d5752d4462e28bdd13ad029
CarlosEstrada10 <estrada.salas.carlos.miguel@gmail.com>
2025-03-13 23:22:06 -0600

    Regresando a versiones posteriores

--- src/interfaz.py
+++ src/interfaz.py
@@ -1,19 +1,6 @@
 import os
-import sys
 import subprocess
 from tkinter import Tk, Button, Label, filedialog
-
-def resource_path(relative_path):
-    """
-    Obtiene la ruta absoluta del recurso, ya sea en modo desarrollo o empaquetado.
-    En modo desarrollo se utiliza el directorio donde está este archivo.
-    En modo "congelado" se usa sys._MEIPASS.
-    """
-    try:
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_path, relative_path)
 
 def seleccionar_carpeta():
     return filedialog.askdirectory(title="Selecciona la carpeta de trabajo")
@@ -21,9 +8,8 @@ def seleccionar_carpeta():
 def actualizar_archivos():
     carpeta = seleccionar_carpeta()
     if carpeta:
-        # En desarrollo y producción, 'actualizar.py' se encontrará en el mismo directorio.
-        script_path = resource_path("actualizar.py")
-        subprocess.run([sys.executable, script_path, carpeta])
+        # Llama al script actualizar_xls_a_xlsx.py pasando la carpeta
+        subprocess.run(["python", "src/actualizar.py", carpeta])
         label_result.config(text="Actualización completada.")
     else:
         label_result.config(text="No se seleccionó carpeta.")
@@ -31,13 +17,13 @@ def actualizar_archivos():
 def extraer_ticket():
     carpeta = seleccionar_carpeta()
     if carpeta:
-        script_path = resource_path("ticket.py")
-        subprocess.run([sys.executable, script_path, carpeta])
+        # Llama al script extraer_ticket.py pasando la carpeta
+        subprocess.run(["python", "src/ticket.py", carpeta])
         label_result.config(text="Ticket generado.")
     else:
         label_result.config(text="No se seleccionó carpeta.")
 
-# Configuración de la interfaz gráfica
+# Configuración de la interfaz
 root = Tk()
 root.title("Generador de Ticket")
 
